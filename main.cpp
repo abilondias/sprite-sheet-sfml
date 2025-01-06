15758d6f62bee9ebb5b755c6f78142f21cae1bff
Abilon Dias <abilon.amd@gmail.com>
2025-01-06 20:50:06 +0200

    feat: add background sprite and improved movement handling

--- main.cpp
+++ main.cpp
@@ -1,97 +1,134 @@
 #include <SFML/Graphics.hpp>
+#include <cmath>
 
-class Player {
-public:
-    // assets
-    sf::Texture idleStrip;
-    sf::Sprite sprite;
-
-    // transforms
-    enum Direction { Left, Right};
-    Direction direction;
-
-    enum Status {
-        Idle = 0,
-        WalkingUp = 1,
-        Walking = 2
+namespace Game {
+    class Movement {
+    public:
+        static constexpr sf::Vector2f up = {0, -1};
+        static constexpr sf::Vector2f down = {0, 1};
+        static constexpr sf::Vector2f left = {-1, 0};
+        static constexpr sf::Vector2f right = {1, 0};
     };
 
-    Status status;
+    class Player {
+    public:
+        sf::Texture textureSheet;
+        sf::Sprite sprite;
 
-    sf::Clock animationClock;
-    int currentFrame;
-    int msFrameDuration;
+        sf::Vector2f position;
 
-    const sf::Vector2i spriteSize{24, 32};
+        enum Direction { Left, Right };
+        Direction direction;
 
-    sf::Vector2f position;
+        enum Status { Idle = 0, WalkingUp = 1, Walking = 2 };
+        Status status;
 
-    Player() : idleStrip("../../assets/image/brawler/sheet.png"),
-                sprite(idleStrip), position(400, 300) {
-        currentFrame = 0;
-        msFrameDuration = 300;
-        direction = Right;
+        // animation
+        sf::Clock animationClock;
+        int currentFrame;
+        const int msFrameDuration = 250;
+        const sf::Vector2i spriteSize{24, 32};
+        const float spriteScale;
 
-        sprite.setTextureRect(sf::IntRect({0, 0}, spriteSize));
-        sprite.setOrigin({12, 16});
-        sprite.setScale({2.5f, 2.5f});
-    }
+        const short animationFrameCount = 4; // all used animations have 4 frames
 
-    void update(float delta) {
-        status = Idle;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
-            direction = Left;
-            sprite.setScale({-2.5f, 2.5f});
-            position.x -= 2.f;
-            status = Walking;
-        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+        const float speed = 80;
+
+        Player(const float scale) :
+            textureSheet("../../assets/image/brawler/sheet.png"), sprite(textureSheet), position(100, 500),
+            spriteScale(scale) {
+            currentFrame = 0;
             direction = Right;
-            sprite.setScale({2.5f, 2.5f});
-            position.x += 2.f;
-            status = Walking;
+
+            sprite.setTextureRect(sf::IntRect({0, 0}, spriteSize));
+            sprite.setOrigin({12, 16});
+
+            sprite.setScale({spriteScale, spriteScale});
         }
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
-            position.y -= 1.2f;
-            status = WalkingUp;
-        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
-            position.y += 1.2f;
-            status = Walking;
+        void update(float delta) {
+            status = Idle;
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+                direction = Left;
+                sprite.setScale({-1 * spriteScale, spriteScale});
+                position += Movement::left * speed * delta;
+                status = Walking;
+            } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+                direction = Right;
+                sprite.setScale({spriteScale, spriteScale});
+                position += Movement::right * speed * delta;
+                status = Walking;
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+                position += Movement::up * speed * delta;
+                status = WalkingUp;
+            } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+                position += Movement::down * speed * delta;
+                status = Walking;
+            }
+
+            sprite.setPosition(position);
+
+            if (animationClock.getElapsedTime().asMilliseconds() >= msFrameDuration) {
+                animationClock.restart();
+
+                // cycle between sprite segment frames
+                currentFrame = (currentFrame + 1) % animationFrameCount;
+
+                // update the texture rectangle
+                sprite.setTextureRect(sf::IntRect({currentFrame * 24, status * 32}, spriteSize));
+            }
         }
-
-        currentFrame = (currentFrame + 1) % 4;  // Cycle between 0, 1, 2, 3
-
-        sprite.setPosition(position);
-
-        if (animationClock.getElapsedTime().asMilliseconds() >= msFrameDuration) {
-            animationClock.restart();
-
-            // cycle between sprite segment frames
-            currentFrame = (currentFrame + 1) % 4;
-
-            // update the texture rectangle
-            sprite.setTextureRect(sf::IntRect({currentFrame * 24, status * 32}, spriteSize));
-        }
-    }
-};
+    };
+}; // namespace Game
 
 int main() {
-    sf::RenderWindow window = sf::RenderWindow(sf::VideoMode({800u, 600u}), "Sprite Render");
+    constexpr unsigned windowWidth = 800;
+    constexpr unsigned windowHeight = 600;
+
+    sf::RenderWindow window = sf::RenderWindow(sf::VideoMode({windowWidth, windowHeight}), "Sprite Sheet Render");
     window.setFramerateLimit(60);
 
-    auto player = std::make_unique<Player>();
+    const sf::Texture scenarioTexture("../../assets/image/brawler/scenario.png");
+    sf::Sprite scenario(scenarioTexture);
+
+    const float scenarioScale = static_cast<float>(window.getSize().y) / scenarioTexture.getSize().y;
+    scenario.setScale({scenarioScale, scenarioScale});
+
+    const auto player = std::make_unique<Game::Player>(scenarioScale);
+
+    const float windowTextureWidth = window.getSize().x / scenarioScale;
+    sf::IntRect textureRect = {{0, 0}, {int(windowTextureWidth), int(scenarioTexture.getSize().y)}};
 
     sf::Clock clock;
+
     while (window.isOpen()) {
-        float delta = clock.getElapsedTime().asSeconds();
+        float delta = clock.restart().asSeconds();
         while (const auto& e = window.pollEvent()) {
             if (e->is<sf::Event::Closed>()) {
                 window.close();
             }
         }
-        window.clear(sf::Color::White);
+        window.clear();
 
         player->update(delta);
+
+        // simple background and bounds handling by moving the texture rectangle based on the % of the screen
+        // there's still free vertical movement
+        if (player->position.x < 0) {
+            player->position.x = 0;
+        } else if (player->position.x > window.getSize().x) {
+            player->position.x = windowWidth;
+        }
+
+        // ex.: player's x at 400 is 50%, so move half of it's possible distance without going out of the image bounds
+        const float backgroundPercent = player->position.x / windowWidth;
+        textureRect.position.x = backgroundPercent * (scenarioTexture.getSize().x - windowTextureWidth);
+        scenario.setTextureRect(textureRect);
+
+        window.draw(scenario);
         window.draw(player->sprite);
 
         window.display();
