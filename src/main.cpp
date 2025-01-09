f3f1650cd4587dc4a37b3fedce0058e99132af48
Abilon Dias <abilon.amd@gmail.com>
2025-01-09 21:20:15 +0200

    rename and comment bounds resolution

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,10 @@ float interpolate(float x, float t1, float t2, float s1, float s2) {
     return g;
 }
 
-float getScenarioBoundary(const sf::Vector2f& position) {
-    float x = position.x;
+// this is fine at the moment because there's only 1 background/scenario
+// likely to be moved to Background, and defined on initialization for each different one
+// values will need to be scaled based on window/texture size
+float getScenarioVerticalBoundary(const float x) {
     if (x < 30) return 315;
     // interpolate here so it can generate diagonal height values between points 30 and 70
     if (x < 70) return interpolate(x, 30, 70, 315, 360);
@@ -43,8 +45,8 @@ int main() {
     Game::Background background(backgroundTexturePath, window);
     Game::Player player(playerTexturePath, background.scale);
     Game::Enemy enemy(enemyTexturePath, background.scale, player);
-
     Game::Camera camera(player, window, background.width);
+
     sf::Clock clock;
 
     while (window.isOpen()) {
@@ -60,12 +62,12 @@ int main() {
         // update game objects
         player.update(delta);
         float playerX = std::clamp(player.getPosition().x, 0.f, background.width);
-        float playerY = std::clamp(player.getPosition().y, getScenarioBoundary(player.getPosition()), windowHeight);
+        float playerY = std::clamp(player.getPosition().y, getScenarioVerticalBoundary(player.getPosition().x), windowHeight);
         player.setPosition({playerX, playerY});
         camera.update(delta);
         enemy.update(delta);
         float enemyX = std::clamp(enemy.getPosition().x, 0.f, background.width);
-        float enemyY = std::clamp(enemy.getPosition().y, getScenarioBoundary(enemy.getPosition()), windowHeight);
+        float enemyY = std::clamp(enemy.getPosition().y, getScenarioVerticalBoundary(enemy.getPosition().x), windowHeight);
         enemy.setPosition({enemyX, enemyY});
 
         // render
